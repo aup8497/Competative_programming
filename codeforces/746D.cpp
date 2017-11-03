7e861c9acf76843e2dd558f16445e87aeb3b637e
aup8497 <akshayup609@gmail.com>
2017-11-03 09:32:48 +0530

    updated

---
+++ codeforces/746D.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <math.h>
+#include <vector>
+#include <algorithm>
+
+#define pb push_back
+#define mp make_pair
+#define ll long long
+#define MOD 1000000007
+const int MAX=1e5+5;
+using namespace std;
+
+ll n,m,i,j,k;
+
+int main(){
+
+	cin >> n >> k >> a >> b;
+
+	
+
+
+return 0;	
+}
